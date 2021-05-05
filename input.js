71e9d19cc86629d25d28a8b73fd93cf884f32b70
Moses Kim <moseskim4740@gmail.com>
2021-05-05 17:44:26 +0000

    Connected user input to server

--- input.js
+++ input.js
@@ -1,5 +1,12 @@
-const handleUserInput = function () {
-  if (key === '\u0003') {
+let connection;
+
+const handleUserInput = function (key) {
+  if (key === 'w') connection.write('Move: up');
+  else if (key === 'a') connection.write('Move: left');
+  else if (key === 's') connection.write('Move: down');
+  else if (key === 'd') connection.write('Move: right');
+  else if (key === 'c') connection.write('Say: hi');
+  else if (key === '\u0003') {
     process.exit();
   }
 };
@@ -7,7 +14,8 @@ const handleUserInput = function () {
 
 // setup interface to handle user input from stdin
 
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");

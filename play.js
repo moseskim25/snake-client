0816bb78a03d3264fa20efd755d27cb54d4cb0d8
Moses Kim <moseskim4740@gmail.com>
2021-05-05 16:37:15 +0000

    Created a connection with host

---
+++ play.js
@@ -0,0 +1,23 @@
+const net = require("net");
+const {connect} = require('./client');
+
+// establishes a connection with the game server
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '135.23.222.131',// IP address here,
+//     port: 50542// PORT number here,
+//   });
+
+//   conn.on('data', (data) => {
+//     console.log(data);
+//   })
+
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
+
+//   return conn;
+// };
+
+console.log("Connecting ...");
+connect();
+
